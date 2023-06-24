91151f3f75db0c4e517d5a9a7480df74ebc0410f
Ayahiro <Aya13001060ina@gmail.com>
2023-06-24 21:33:56 +0900

    クイズ作成修正

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 class Question extends Model
 {
@@ -36,16 +38,31 @@ class Question extends Model
     // レコードを追加
     public static function add(array $data)
     {
-        $question = new Question();
-        $question->text = $data['question'];
+        $quiz = new self();
+        // 画像アップロード
+        $image = $data['image'];
+        
+        if ($image) {
+            $dir = 'img/quizzes';
+            $image_extension = $image->getClientOriginalExtension();
+            $file_name = uniqid() . '.' . $image_extension;
+            
+            Storage::disk('public')->put($dir . '/' . $file_name, File::get($image));
+            $quiz->image = $file_name;
+        }
+        $quiz->text = $data['question'];
+        $quiz->save();
+
+        $quiz_id = $quiz->id;
 
         foreach ($data['option'] as $key => $option) {
-            $question->options()->create([
+            $quiz->options()->create([
+                'question_id' => $quiz_id,
                 'text' => $option,
                 'is_correct' => ((int)$data['answer'] === $key + 1),
             ]);
         };
 
-        return $question;
+        return;
     }
 }
